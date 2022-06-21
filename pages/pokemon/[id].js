6e2dc5ce5b5be2329742548b71ec47d16185826f
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-06-21 16:31:17 -0300

    Add files via upload

---
+++ pages/pokemon/[id].js
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function Pokemon({ pokemon }) {
+  return (
+    <div>
+      <h1>{pokemon.name}</h1>
+      <img src={pokemon.sprites.front_default} alt="Imagem de um pokémon" />
+    </div>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+    .then((respostaDoServer) => {
+      if (respostaDoServer.ok) {
+        return respostaDoServer.json();
+      }
+    })
+    .then((respostaEmObjeto) => respostaEmObjeto);
+
+  return {
+    props: {
+      pokemon,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          id: "1",
+        },
+      },
+      {
+        params: {
+          id: "2",
+        },
+      },
+      {
+        params: {
+          id: "3",
+        },
+      },
+      {
+        params: {
+          id: "4",
+        },
+      },
+      {
+        params: {
+          id: "5",
+        },
+      },
+    ],
+    fallback: false,
+  };
+}
